edee44d346941335113a337ceee850acb70a2f91
DeMo900 <dodoadam893@gmail.com>
2024-06-06 12:55:21 +0300

    mongo db setup

--- app.js
+++ app.js
@@ -2,11 +2,36 @@
 
 const bodyParser = require("body-parser")
 const express = require("express")
-const mongoose = require("mongoose")
+const mongo = require("mongoose")
 
 let app = express()
 //middlewares
 app.use(express.json())
 app.set(`view engine`,`ejs`)
 app.use(bodyParser)
+app.use(bodyParser.urlencoded({extended:true}))
+//data base 
+mongo.connect("mongodb+srv://dodoadam893:AdAm233@cluster0.eoito82.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+
+console.log("conected to db")
+}).catch((err)=>{
+    console.log(`error from database ${err}`)
+})
+//signup schema
+let signup = new mongo.Schema({
+    name:String,
+    age:Number,
+    password:String,
+    lastname:String
+})
+
+let signupmodel = mongo.model("signup",signup)
+
+//login schema
+let login = new mongo.Schema({
+    name:String,
+    password:String
+})
+
+let loginmodel = mongo.model("login",login)
 
